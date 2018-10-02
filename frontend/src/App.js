886cb03b42d97ba443a8711e1947836644cea6c3
stark <choi-0202@kaist.ac.kr>
2018-10-02 12:40:18 +0900

    add frontend with create-react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  
+  componentDidMount() {
+    const script1 = document.createElement("script");
+    const script2 = document.createElement("script");
+    const script3 = document.createElement("script");
+        
+    script1.src = "./js/controller.js";
+    script1.async = true;
+    script2.src = "./js/render.js";
+    script2.async = true;
+    script3.src = "./js/tetris.js";
+    script3.async = true;
+
+    document.body.appendChild(script1);
+    document.body.appendChild(script2);
+    document.body.appendChild(script3);
+  }
+
+
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <canvas width='300' height='600'></canvas>
+        <button id="playbutton" onClick={() => playButtonClicked()}>Play</button>
       </div>
     );
   }
